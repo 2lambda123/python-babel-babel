e01ec551407c7734dc9d5212bd9f0d8f78380bbc
Tomas Roun <tomas.roun8@gmail.com>
2024-09-22 15:21:17 +0200

    Allow specifying an explicit format in parse_date/parse_time

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -643,6 +643,13 @@ def test_parse_date():
     assert dates.parse_date('2004-04-01', locale='sv_SE', format='short') == date(2004, 4, 1)
 
 
+def test_parse_date_custom_format():
+    assert dates.parse_date('1.4.2024', format='dd.mm.yyyy') == date(2024, 4, 1)
+    assert dates.parse_date('2024.4.1', format='yyyy.mm.dd') == date(2024, 4, 1)
+    # Dates that look like ISO 8601 should use the custom format as well:
+    assert dates.parse_date('2024-04-01', format='yyyy.dd.mm') == date(2024, 1, 4)
+
+
 @pytest.mark.parametrize('input, expected', [
     # base case, fully qualified time
     ('15:30:00', time(15, 30)),
@@ -664,6 +671,11 @@ def test_parse_date():
 ])
 def test_parse_time(input, expected):
     assert dates.parse_time(input, locale='en_US') == expected
+
+
+def test_parse_time_custom_format():
+    assert dates.parse_time('15:30:00', format='HH:mm:ss') == time(15, 30)
+    assert dates.parse_time('00:30:15', format='ss:mm:HH') == time(15, 30)
 
 
 @pytest.mark.parametrize('case', ['', 'a', 'aaa'])
